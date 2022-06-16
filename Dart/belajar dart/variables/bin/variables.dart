78bca92aead84cb0882d02c2a62dba6ba4014f04
Guido Alfino Kiu <69782712+GuidoKiu@users.noreply.github.com>
2022-06-16 10:53:31 +0800

    Variables

---
+++ Dart/belajar dart/variables/bin/variables.dart
@@ -0,0 +1,17 @@
+import 'package:variables/variables.dart' as variables;
+
+void main(List<String> arguments) {
+  var greetings = "Hello World";
+
+  // Deklarasi
+  var a;
+  // Inisialisasi
+  // assignment operator (=)
+  a = "Hello Indonesia";
+
+  var variabelNull; // Nilai-nya null
+
+  print(a);
+  print(greetings);
+  print(variabelNull);
+}
