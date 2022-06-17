88fb60fd544bcf74667f908aefdb437173612d26
Guido Alfino Kiu <69782712+GuidoKiu@users.noreply.github.com>
2022-06-17 13:54:57 +0800

    Operators

---
+++ Dart/belajar dart/operators/bin/operators.dart
@@ -0,0 +1,21 @@
+void main(List<String> arguments) {
+  // Operator Assignment (=)
+
+  // Operator Matematika (+; -, *, /, ~/, %)
+  // Perbedaan / (menghasilkan output double) dan ~/ (menghasilkan output integer)
+  print(2 / 3);
+  print(2 ~/ 3);
+
+  // Increment dan decrement
+  int a = 3;
+  a++;
+  print("");
+  print(a++);
+  print(a--);
+  print(a *= 2);
+  print(a -= 2);
+
+  // Operator Perbandingan (==, !=, <, >, <=, >=)
+
+  // Operator Logika (&&, ||, !)
+}
