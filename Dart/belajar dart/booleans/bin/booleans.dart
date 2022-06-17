5878222cc3cb0a46bce06bd2da67ca568a2cdf93
Guido Alfino Kiu <69782712+GuidoKiu@users.noreply.github.com>
2022-06-17 13:42:53 +0800

    Booleans

---
+++ Dart/belajar dart/booleans/bin/booleans.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  // Penemu George Boole (ahli aljbar)
+
+  bool benar = true;
+  bool salah = false;
+
+  print(benar);
+  print(salah);
+
+  // Penggunaan operator NOT atau BANG
+  print(!benar);
+  print(!salah);
+}
