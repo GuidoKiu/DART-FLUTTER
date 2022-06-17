d5712c827017f7de94971a28e8df8027d8fdf5f9
Guido Alfino Kiu <69782712+GuidoKiu@users.noreply.github.com>
2022-06-17 16:10:03 +0800

    Null Safety

---
+++ Dart/belajar dart/null_safety/bin/null_safety.dart
@@ -0,0 +1,12 @@
+// Tujuan dari Null Safety untuk memastikan nilai dari variable itu tidak null
+// Kecuali variable tersebut secara eksplisit memiliki nilai default
+
+void main() {
+  String? favoriteFood = 'Mie Ayam';
+
+  buyAMeal(favoriteFood!);
+}
+
+void buyAMeal(String favoriteFood) {
+  print('Bought $favoriteFood');
+}
