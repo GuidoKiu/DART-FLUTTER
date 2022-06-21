000a8bf2e5338ac7016e2eb13369c98163bb712c
Guido Alfino Kiu <69782712+GuidoKiu@users.noreply.github.com>
2022-06-21 20:47:11 +0800

    Extension Methods

---
+++ Dart/Object Oriented Programming/extension_methods/bin/extension_methods.dart
@@ -0,0 +1,8 @@
+import 'extension.dart';
+
+void main(List<String> arguments) {
+  var unsortedNumbers = [2, 5, 3, 1, 4];
+  print(unsortedNumbers);
+  var sortedNumbers = unsortedNumbers.sortAsc();
+  print(sortedNumbers);
+}
